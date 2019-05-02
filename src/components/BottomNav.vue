caf29e0b7548ce3cf67bec1a74c757185942ab2a
Pama <burinpama@gmail.com>
2019-05-02 21:07:48 +0700

    initial commit

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container pa-0>
+    <v-bottom-nav :active.sync="bottomNav" :value="true" absolute color="transparent">
+        <v-btn color="orange" flat @click="$router.push('./')">
+          <v-icon>home</v-icon>
+        </v-btn>
+
+        <v-btn color="orange" flat @click="$router.push('./mapdriver')">
+          <v-icon>place</v-icon>
+        </v-btn>
+
+        <v-btn color="orange" flat @click="$router.push('./statusdriver')">
+          <v-icon>query_builder</v-icon>
+        </v-btn>
+
+        <v-btn color="orange" flat @click="$router.push('./profile')">
+          <v-icon>person</v-icon>
+        </v-btn>
+      </v-bottom-nav>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "bottom-nav"
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/MapDriver.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container pa-0>
+    <v-layout column>
+      <v-flex mb-2>
+        <top-nav/>
+      </v-flex>
+      <v-flex>
+        <v-parallax
+          dark
+          src="https://developers.google.com/maps/documentation/android-sdk/images/zoom-level-14.png"
+        ></v-parallax>
+      </v-flex>
+
+      <v-flex>
+        <bottom-nav/>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import TopNav from "../components/TopNav";
+import BottomNav from "../components/BottomNav";
+
+export default {
+  name: "map-driver",
+  components: {
+    TopNav,
+    BottomNav
+  }
+};
+</script>
